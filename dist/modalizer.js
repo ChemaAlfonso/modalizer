ce61980d97b98152dbb5e10354ec8684e17d6a10
Chema Alfonso <hola@chemaalfonso.com>
2023-10-31 12:21:59 +0100

    rebuild

--- dist/modalizer.js
+++ dist/modalizer.js
@@ -1,1 +1,148 @@
-var t,e;!function(t){t.FADE_IN="fadeIn",t.FADE_OUT="fadeOut"}(t||(t={})),function(t){t.OPENING="opening",t.OPENED="opened",t.CLOSING="closing",t.CLOSED="closed"}(e||(e={}));class i{constructor(t){this.openedModalizations=[],this.modalizations=new Map,this.modalize(t),this.listen()}closeLast(){this.openedModalizations.length&&this.hideModalized(this.openedModalizations[this.openedModalizations.length-1])}add(t){this.modalize(t)}reset(t=!0){this.modalizations.forEach((e=>{e.target.close(),t&&e.target.parentElement?.insertBefore(e.target.firstChild,e.target),e.target.parentElement?.removeChild(e.target)})),this.modalizations.clear(),this.openedModalizations=[]}modalize(t=[]){t.forEach((({element:t,trigger:i,config:a})=>{const n={target:this.insertElementIntoModalizedTarget(t),trigger:i,config:a?{...this.defaultConfig(),...a}:this.defaultConfig(),state:e.CLOSED};this.initializeModalized(n),this.modalizations.set(i,n)}))}insertElementIntoModalizedTarget(t){const e=t?.parentElement||document.body,i=document.createElement("dialog");return i.classList.add("modalizer"),e.insertBefore(i,t),i.appendChild(t),i}initializeModalized(t){const{target:i,config:a}=t;a?.closer&&a.closer.addEventListener("click",(()=>this.hideModalized(t))),a?.customClassName&&i.classList.add(a.customClassName),i.addEventListener("animationend",(i=>{const{animationName:n,pseudoElement:o}=i;o||(n!==a.animationIn||t.state!==e.OPENING?n!==a.animationOut||t.state!==e.CLOSING||this.closeModalized(t):this.openModalized(t))})),i.addEventListener("cancel",(t=>{t.preventDefault()})),t.target.classList.add("modalizer--initialized")}listen(){document.addEventListener("click",(t=>{const e=t.target;if(!e)return;const i=this.modalizations.get(e);i&&this.showModalized(i)})),document.addEventListener("keydown",(t=>{if("Escape"!==t.code||!this.openedModalizations.length)return;const e=this.openedModalizations[this.openedModalizations.length-1];e.config.closeOnEscKeyPress&&this.hideModalized(e)}))}showModalized(t){t.state===e.CLOSED&&(t.config?.animationIn&&t.target.classList.add(t.config.animationIn),t.target.showModal(),t.target.setAttribute("opening",""),t.state=e.OPENING,this.openedModalizations.push(t))}openModalized(t){t.state===e.OPENING&&(t.target.removeAttribute("opening"),t.state=e.OPENED,t.config?.animationIn&&t.target.classList.remove(t.config.animationIn))}hideModalized(t){t.state===e.OPENED&&(t.target.setAttribute("closing",""),t.state=e.CLOSING,t.config?.animationOut&&t.target.classList.add(t.config.animationOut))}closeModalized(t){t.state===e.CLOSING&&(t.target.close(),t.target.removeAttribute("closing"),t.state=e.CLOSED,t.config?.animationOut&&t.target.classList.remove(t.config.animationOut),this.openedModalizations.pop())}defaultConfig(){return{animationIn:t.FADE_IN,animationOut:t.FADE_OUT,closeOnEscKeyPress:!0}}}export{i as Modalizer};
+var MODALIZER_ANIMATION;
+(function (MODALIZER_ANIMATION) {
+    MODALIZER_ANIMATION["FADE_IN"] = "fadeIn";
+    MODALIZER_ANIMATION["FADE_OUT"] = "fadeOut";
+})(MODALIZER_ANIMATION || (MODALIZER_ANIMATION = {}));
+var MODALIZER_STATE;
+(function (MODALIZER_STATE) {
+    MODALIZER_STATE["OPENING"] = "opening";
+    MODALIZER_STATE["OPENED"] = "opened";
+    MODALIZER_STATE["CLOSING"] = "closing";
+    MODALIZER_STATE["CLOSED"] = "closed";
+})(MODALIZER_STATE || (MODALIZER_STATE = {}));
+class Modalizer {
+    constructor(modalizables) {
+        this.openedModalizations = [];
+        this.modalizations = new Map();
+        this.modalize(modalizables);
+        this.listen();
+    }
+    closeLast() {
+        if (!this.openedModalizations.length)
+            return;
+        this.hideModalized(this.openedModalizations[this.openedModalizations.length - 1]);
+    }
+    add(modalizables) {
+        this.modalize(modalizables);
+    }
+    reset(keepContent = true) {
+        this.modalizations.forEach(modalization => {
+            modalization.target.close();
+            if (keepContent)
+                modalization.target.parentElement?.insertBefore(modalization.target.firstChild, modalization.target);
+            modalization.target.parentElement?.removeChild(modalization.target);
+        });
+        this.modalizations.clear();
+        this.openedModalizations = [];
+    }
+    // Generate
+    modalize(modalizables = []) {
+        modalizables.forEach(({ element, trigger, config }) => {
+            const modalized = {
+                target: this.insertElementIntoModalizedTarget(element),
+                trigger,
+                config: config ? { ...this.defaultConfig(), ...config } : this.defaultConfig(),
+                state: MODALIZER_STATE.CLOSED
+            };
+            this.initializeModalized(modalized);
+            this.modalizations.set(trigger, modalized);
+        });
+    }
+    insertElementIntoModalizedTarget(element) {
+        const modalizerRoot = element?.parentElement || document.body;
+        const modalizedElement = document.createElement('dialog');
+        modalizedElement.classList.add('modalizer');
+        modalizerRoot.insertBefore(modalizedElement, element);
+        modalizedElement.appendChild(element);
+        return modalizedElement;
+    }
+    // Initialize
+    initializeModalized(modalized) {
+        const { target, config } = modalized;
+        if (config?.closer)
+            config.closer.addEventListener('click', () => this.hideModalized(modalized));
+        if (config?.customClassName)
+            target.classList.add(config.customClassName);
+        target.addEventListener('animationend', e => {
+            const { animationName, pseudoElement } = e;
+            if (pseudoElement)
+                return;
+            if (animationName === config.animationIn && modalized.state === MODALIZER_STATE.OPENING) {
+                this.openModalized(modalized);
+                return;
+            }
+            if (animationName === config.animationOut && modalized.state === MODALIZER_STATE.CLOSING) {
+                this.closeModalized(modalized);
+                return;
+            }
+        });
+        target.addEventListener('cancel', e => {
+            e.preventDefault();
+        });
+        modalized.target.classList.add('modalizer--initialized');
+    }
+    listen() {
+        document.addEventListener('click', e => {
+            const target = e.target;
+            if (!target)
+                return;
+            const modalized = this.modalizations.get(target);
+            if (!modalized)
+                return;
+            this.showModalized(modalized);
+        });
+        document.addEventListener('keydown', e => {
+            if (e.code !== 'Escape' || !this.openedModalizations.length)
+                return;
+            const lastModalizedOpened = this.openedModalizations[this.openedModalizations.length - 1];
+            if (!lastModalizedOpened.config.closeOnEscKeyPress)
+                return;
+            this.hideModalized(lastModalizedOpened);
+        });
+    }
+    // Behaviours
+    showModalized(modalized) {
+        if (modalized.state !== MODALIZER_STATE.CLOSED)
+            return;
+        if (modalized.config?.animationIn)
+            modalized.target.classList.add(modalized.config.animationIn);
+        modalized.target.showModal();
+        modalized.target.setAttribute('opening', '');
+        modalized.state = MODALIZER_STATE.OPENING;
+        this.openedModalizations.push(modalized);
+    }
+    openModalized(modalized) {
+        if (modalized.state !== MODALIZER_STATE.OPENING)
+            return;
+        modalized.target.removeAttribute('opening');
+        modalized.state = MODALIZER_STATE.OPENED;
+        if (modalized.config?.animationIn)
+            modalized.target.classList.remove(modalized.config.animationIn);
+    }
+    hideModalized(modalized) {
+        if (modalized.state !== MODALIZER_STATE.OPENED)
+            return;
+        modalized.target.setAttribute('closing', '');
+        modalized.state = MODALIZER_STATE.CLOSING;
+        if (modalized.config?.animationOut)
+            modalized.target.classList.add(modalized.config.animationOut);
+    }
+    closeModalized(modalized) {
+        if (modalized.state !== MODALIZER_STATE.CLOSING)
+            return;
+        modalized.target.close();
+        modalized.target.removeAttribute('closing');
+        modalized.state = MODALIZER_STATE.CLOSED;
+        if (modalized.config?.animationOut)
+            modalized.target.classList.remove(modalized.config.animationOut);
+        this.openedModalizations.pop();
+    }
+    // Default
+    defaultConfig() {
+        return {
+            animationIn: MODALIZER_ANIMATION.FADE_IN,
+            animationOut: MODALIZER_ANIMATION.FADE_OUT,
+            closeOnEscKeyPress: true
+        };
+    }
+}export{Modalizer};
