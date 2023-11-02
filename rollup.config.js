9743db8e8830734dae51b4831c337ee1610ac9d5
Chema Alfonso <hola@chemaalfonso.com>
2023-11-02 10:04:30 +0100

    feature: created animateAnimation enums

--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,35 @@
 import typescript from '@rollup/plugin-typescript'
 import terser from '@rollup/plugin-terser'
 
-export default {
-	input: 'src/modalizer.ts',
-	output: [
-		{
-			file: 'dist/modalizer.js',
-			format: 'es'
-		},
-		{
-			file: 'dist/modalizer.min.js',
-			format: 'es',
-			plugins: [terser()]
-		}
-	],
-	plugins: [typescript()]
-}
+export default [
+	{
+		input: 'src/modalizer.ts',
+		output: [
+			{
+				file: 'dist/modalizer.js',
+				format: 'es'
+			},
+			{
+				file: 'dist/modalizer.min.js',
+				format: 'es',
+				plugins: [terser()]
+			}
+		],
+		plugins: [typescript()]
+	},
+	{
+		input: 'src/animateAnimations.ts',
+		output: [
+			{
+				file: 'dist/animateAnimations.js',
+				format: 'es'
+			},
+			{
+				file: 'dist/animateAnimations.min.js',
+				format: 'es',
+				plugins: [terser()]
+			}
+		],
+		plugins: [typescript()]
+	}
+]
